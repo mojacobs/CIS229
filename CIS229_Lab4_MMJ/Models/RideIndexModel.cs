9fab3ab11bc9e31cee32e84c806fb798a08eee06
Will Darnell <will@ogsys.com>
2017-02-05 19:40:28 -0800

    refactor to use a more complex model, allowing for a search form with dropdowns

---
+++ CIS229_Lab4_MMJ/Models/RideIndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CIS229_Lab4_MMJ.Models
+{
+    public class RideIndexModel
+    {
+        public IEnumerable<Ride> Rides { get; set; }
+
+        public IEnumerable<SelectListItem> Campuses { get; set; }
+
+        public int SelectedCampusId { get; set; }
+    }
+}
